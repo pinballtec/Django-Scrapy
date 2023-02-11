6df351f00c49a09ef0c2a1f7a7a35a4eecd7b7ca
Daniil_Boika <daniilboiko99@gmail.com>
2023-02-11 12:01:46 +0100

    added filter feature

--- scraping_service/scrapping/views.py
+++ scraping_service/scrapping/views.py
@@ -6,6 +6,16 @@ from .models import Job_Offers
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        qs = Job_Offers.objects.all()
-        context = {'object_list': qs}
-        return render(request, 'scrapping/home.html', context)
+        print(request.GET)
+        city = request.GET.get('city')
+        programming_language = request.GET.get('p_language')
+        qs = []
+        if city or programming_language:
+            """Realization of filtering"""
+            _filter = {}
+            if city:
+                _filter['city__name'] = city
+            if programming_language:
+                _filter['language__name'] = programming_language
+            qs = Job_Offers.objects.filter(**_filter)
+        return render(request, 'scrapping/home.html', {'object_list': qs})
