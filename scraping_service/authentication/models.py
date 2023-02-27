1d3b944fded09be7a2ae3296238a2fd0981f622e
Daniil_Boika <daniilboiko99@gmail.com>
2023-02-27 20:18:06 +0100

    model reconfigured

--- scraping_service/authentication/models.py
+++ scraping_service/authentication/models.py
@@ -1,4 +1,71 @@
 from django.db import models
-
+from scrapping.models import *
 # Create your models here.
 
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL,
+                             null=True,
+                             blank=True)
+    language = models.ForeignKey(Programming_Language,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True)
+
+    newsletter = models.BooleanField(default=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
