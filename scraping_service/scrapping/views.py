d6bbae77271875295e64880778974407c085d80d
Daniil_Boika <daniilboiko99@gmail.com>
2023-03-08 13:22:49 +0100

    pagination

--- scraping_service/scrapping/views.py
+++ scraping_service/scrapping/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from .models import Job_Offers
 from .forms import FindForm
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -15,12 +16,12 @@ class HomeView(View):
 
 
 class ListView(View):
-    def get(self, request, *args, **kwargs):
-        print(request.GET)
+    def get(self, request):
         form = FindForm()
         city = request.GET.get('city')
         programming_language = request.GET.get('p_language')
-        qs = []
+
+        context = {'city': city, 'programming_language': programming_language, 'form': form}
         if city or programming_language:
             """Realization of filtering"""
             _filter = {}
@@ -29,6 +30,11 @@ class ListView(View):
             if programming_language:
                 _filter['language__name'] = programming_language
             qs = Job_Offers.objects.filter(**_filter).order_by('-time_stamp')
+            paginator = Paginator(qs, 10)
+
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+            context['object_list'] = page_obj
         return render(request,
                       'scrapping/list.html',
-                      {'object_list': qs, 'form': form})
+                      context)
