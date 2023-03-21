5e3fa4edb992d9b3221b87dfa8ea8805dd546e22
Daniil_Boika <daniilboiko99@gmail.com>
2023-03-21 20:52:22 +0100

    update password

--- scraping_service/authentication/urls.py
+++ scraping_service/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_page, name='login'),
-    path('logout/', views.logout_page, name='logout')
+    path('logout/', views.logout_page, name='logout'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='authentication/change-password.html',
+                                                                   success_url='/scrapping_app/home/'), name='change_password'),
 ]
-
