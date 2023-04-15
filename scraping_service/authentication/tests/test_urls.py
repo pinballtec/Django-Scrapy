459778c915b17016122d545f8b7da3c65c8902aa
pinballtec <daniilboiko99@gmail.com>
2023-04-15 23:52:17 +0200

    tests for urls

---
+++ scraping_service/authentication/tests/test_urls.py
@@ -0,0 +1,23 @@
+from django.urls import reverse, resolve
+from django.test import RequestFactory, TestCase
+from ..views import register, login_page, logout_page, update_user_info
+
+
+class AuthenticationUrlsTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_register_url_resolves_to_register_view(self):
+        url = reverse('register')
+        view = resolve(url).func
+        self.assertEqual(view, register)
+
+    def test_login_url_resolves_to_login_view(self):
+        url = reverse('login')
+        view = resolve(url).func
+        self.assertEqual(view, login_page)
+
+    def test_logout_url_resolves_to_logout_view(self):
+        url = reverse('logout')
+        view = resolve(url).func
+        self.assertEqual(view, logout_page)
